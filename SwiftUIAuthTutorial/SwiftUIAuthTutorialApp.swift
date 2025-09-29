f58b74b3cc06cf2119fc469f58e6be745c576806
Leonardo Martinez <leomartinezjr@hotmail.com>
2025-09-29 09:50:15 -0300

    Initial Commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Luana Martinez de la Flor on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
